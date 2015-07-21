0ffac0c541972317992f72b274992cfe9b3a3ca9
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2015-07-21 22:39:28 +0200

    GPON-DASAN: fixed setting name in gpon module offline message

--- plugins/LMSGponDasanPlugin/modules/gponoffline.php
+++ plugins/LMSGponDasanPlugin/modules/gponoffline.php
@@ -2,7 +2,7 @@
 
 if (!ConfigHelper::checkConfig('gpon-dasan.enabled')) {
 	$layout['pagetitle'] = 'GPON';
-	$body = '<P>Moduł GPON jest wyłączony. Włącz w konfiguracji opcję <b>gpon</b></P>';
+	$body = '<P>Moduł GPON jest wyłączony. Włącz w konfiguracji opcję <b>gpon-dasan.enabled</b></P>';
 
 	$SMARTY->assign('body',$body);
 	$SMARTY->display('dialog.html');
