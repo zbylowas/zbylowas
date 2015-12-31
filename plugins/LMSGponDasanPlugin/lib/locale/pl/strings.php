903457e41d973c9a2a98404d5a836f20591c3091
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2015-12-31 18:35:23 +0100

    GPON-DASAN: visual fixes for gpon onu tab

--- plugins/LMSGponDasanPlugin/lib/locale/pl/strings.php
+++ plugins/LMSGponDasanPlugin/lib/locale/pl/strings.php
@@ -1,5 +1,12 @@
 <?php
 
 $_LANG['GPON Dasan Hardware Support'] = 'Obsługa sprzętu GPON firmy Dasan';
+$_LANG['ONU Port Count:'] = 'Liczba portów ONU:';
+$_LANG['OLT Port:'] = 'Port OLT:';
+$_LANG['Profile:'] = 'Profile:';
+$_LANG['Show parameters'] = 'Pokaż parametry';
+$_LANG['ONU ($a):'] = 'ONU ($a):';
+$_LANG['ONU description:'] = 'Opis ONU:';
+$_LANG['ONU ID:'] = 'Id ONU:';
 
 ?>
