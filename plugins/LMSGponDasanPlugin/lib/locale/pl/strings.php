d095316c789cab4576e6ded0e4cb2ac8af4e4f8d
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2016-07-04 22:30:19 +0200

    GPON-DASAN: small ui message change

--- plugins/LMSGponDasanPlugin/lib/locale/pl/strings.php
+++ plugins/LMSGponDasanPlugin/lib/locale/pl/strings.php
@@ -20,7 +20,7 @@ $_LANG['Enter XML configuration template contents (optional)'] = 'Wprowadź zawa
 $_LANG['Are you sure you want to disconnect ONU?'] = 'Jesteś pewien, że chcesz rozłączyć ONU?';
 $_LANG['Are you sure you want to make XML provisioning for ONU?'] = 'Jesteś pewien, że chcesz wykonać provisioning XML dla ONU?';
 $_LANG['Are you sure you want to reset ONU?'] = 'Jesteś pewien, że chcesz wykonać restart ONU?';
-$_LANG['Disconnect ONU'] = 'Rozłącz ONU';
+$_LANG['<!gpon-dasan>Disconnect'] = 'Rozłącz';
 $_LANG['XML provisioning'] = 'Provisioning XML';
 $_LANG['<!gpon-dasan>Reset'] = 'Restart';
 
