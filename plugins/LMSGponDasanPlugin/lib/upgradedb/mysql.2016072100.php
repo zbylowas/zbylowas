a70837e39e24a6ca132094e345bea63ea9f51d05
Tomasz Chiliński <tomasz.chilinski@chilan.com>
2016-07-21 11:34:17 +0200

    GPON-DASAN: fixed mysql schema gpononumodels.xmltemplate field type to mediumtext (fix)

--- plugins/LMSGponDasanPlugin/lib/upgradedb/mysql.2016072100.php
+++ plugins/LMSGponDasanPlugin/lib/upgradedb/mysql.2016072100.php
@@ -2,7 +2,7 @@
 
 $this->BeginTrans();
 
-$this->Execute("ALTER TABLE gpononumodels CHANGE xmltemplate xmltemplate DEFAULT ''");
+$this->Execute("ALTER TABLE gpononumodels CHANGE xmltemplate xmltemplate mediumtext DEFAULT ''");
 
 $this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2016072100', 'dbversion_LMSGponDasanPlugin'));
 
